f1bb754613ee22ae816b4378554b611e12382d6c
Yousef abdelatif <63363268+yousefabdelatif@users.noreply.github.com>
2025-08-04 18:43:15 +0300

    intial ui is set

--- ltx_editor/main.py
+++ ltx_editor/main.py
@@ -1,12 +1,14 @@
 import sys
-from PySide6.QtWidgets import QApplication
-from .ui.main_window import MainWindow
+from PySide6.QtWidgets import *
+from ltx_editor.ui.main_window import MainWindow
 
-def main():
+
+
+if __name__ == "__main__":
     app = QApplication(sys.argv)
-    window = MainWindow()
-    window.show()
-    sys.exit(app.exec())
 
-if __name__ == '__main__':
-    main()
+    appWindow = MainWindow()
+
+    appWindow.show()
+
+    sys.exit(app.exec())
